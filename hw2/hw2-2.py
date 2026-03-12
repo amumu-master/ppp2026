b50ca1fc3e6ee45fd9aa0916f742d557b49ab769
amumu-master <202525236@jbnu.ac.kr>
2026-03-12 13:11:38 +0900

    Delete hw2 directory

--- hw2/hw2-2.py
+++
@@ -1,4 +0,0 @@
-W=float(input("몸무게를 입력하시오 : "))
-H=float(input("키를 입력하시오 : "))*0.01
-BMI=W/H**2
-print(f"당신의 BMI 지수 : {BMI}")
--- hw2/hw2-5.py
+++
@@ -1,3 +0,0 @@
-price=2000
-discount=int(2000*85/100)
-print(f"할인된 가격: {discount}원")
