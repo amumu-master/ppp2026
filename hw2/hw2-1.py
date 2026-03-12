12de9a7d1e0239dbc1e4f448a89a74573dca6b7d
amumu-master <202525236@jbnu.ac.kr>
2026-03-12 13:12:31 +0900

    Add files via upload

---
+++ hw2/hw2-1.py
@@ -0,0 +1,6 @@
+temp_c1=30
+temp_f1=(temp_c1*1.8)+32
+print(f"섭씨:{temp_c1}->화씨:{temp_f1}")
+temp_c2=0
+temp_f2=(temp_c2*1.8)+32
+print(f"섭씨:{temp_c2}->화씨:{temp_f2}")
---
+++ hw2/hw2-5.py
@@ -0,0 +1,3 @@
+price=2000
+discount=int(2000*85/100)
+print(f"할인된 가격: {discount}원")
